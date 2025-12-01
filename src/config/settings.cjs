24875353af336deab386c0ba504066abc90db8b9
Ransara Devnath (unofficial) <amransaramax@gmail.com>
2025-12-01 07:49:59 +0530

    Add configuration settings for the bot

---
+++ src/config/settings.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  "PREFIX": ".",
+  "OWNERS": [
+    "94726880784@s.whatsapp.net"
+  ],
+  "PORT": "3000",
+  "AUTOREADSTATUS": true,
+  "MODE": "public",
+  "BOTNUMBER": "94717775628",
+  "OMDBAPI_KEY": "76cb7f39",
+  "READCMD": true,
+  "AUTOVOICE": false,
+  "AUTOSTICKER": true,
+  "AUTOREPLY": true,
+  "AUTOREACT": false,
+  "WELCOME": false,
+  "ANTIBAD": true,
+  "ANTIBOT": true,
+  "ANTILINK": false,
+  "ANTICALL": false,
+  "ANTIDELETE": true,
+  "ALLWAYSONLINE": true,
+  "MOROCCOBLOCK": true,
+  "OWNERNAME": "𝓜𝓡. 𝓡𝓪𝓷𝓼𝓪𝓻𝓪 𝓓𝓮𝓿𝓷𝓪𝓽𝓱",
+  "NEWSJID": "120363404412030736@g.us",
+  "MOVIEJID": false,
+  "ANIMEJID": "120363404412030736@g.us",
+  "AUTOTYPING": false,
+  "AUTORECORDING": true,
+  "AUTONEWS": false,
+  "OWNERREACT": false,
+  "EMOJI": "👻",
+  "AIMODE": false,
+  "BANNED": [],
+  "SUDO": [
+    "94741852787"
+  ]
+};
